529948e1ac6afb870757979bd888d7f49ac38ada
M3D5 <pablohdzjr@gmail.com>
2020-05-01 18:46:52 -0500

    question blank set ready

--- develop/script.js
+++ develop/script.js
@@ -1,8 +1,19 @@
-let questions = [{
+let questions = [
+  {
     question: "This is question 1",
-    choiceA: "this is the right answer"
-    choiceB: "this is the wrong answer"
-    choiceC: "this is the wrong answer"
-    choiceD: "this is the wrong answer"
-    correct: "A"}
-]
+    choiceA: "this is the right answer",
+    choiceB: "this is the wrong answer",
+    choiceC: "this is the wrong answer",
+    choiceD: "this is the wrong answer",
+    correct: "A",
+  },
+  {
+    question: "This is question 2",
+    choiceA: "this is the wrong answer",
+    choiceB: "this is the wrong answer",
+    choiceC: "this is the right answer",
+    choiceD: "this is the wrong answer",
+    correct: "C",
+  },
+  question,
+];
