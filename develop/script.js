fc4afcefa7d1c495e01c27f9bfab42ae4a7ace0f
M3D5 <pablohdzjr@gmail.com>
2020-05-01 18:28:35 -0500

    started adding questions in js

---
+++ develop/script.js
@@ -0,0 +1,8 @@
+let questions = [{
+    question: "This is question 1",
+    choiceA: "this is the right answer"
+    choiceB: "this is the wrong answer"
+    choiceC: "this is the wrong answer"
+    choiceD: "this is the wrong answer"
+    correct: "A"}
+]
