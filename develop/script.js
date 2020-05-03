693ce451d3e8e24bfd61a06980b58a3127ca1829
M3D5 <pablohdzjr@gmail.com>
2020-05-03 11:18:49 -0500

    started counter and intro

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,18 @@
+const start = document.getElementById("start-btn  ");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+const counter = document.getElementById("counter")
+
+const choiceD
+
+start.addEventListener("click", startQ uiz);
+
+function startQuiz(){
+
+
 let questions = [
   {
     question: "This is question 1",
@@ -15,5 +30,28 @@ let questions = [
     choiceD: "this is the wrong answer",
     correct: "C",
   },
-  question,
+  {
+    question: "This is question 3",
+    choiceA: "this is the wrong answer",
+    choiceB: "this is the wrong answer",
+    choiceC: "this is the wrong answer",
+    choiceD: "this is the right answer",
+    correct: "D",
+  },
+  {
+    question: "This is question 4",
+    choiceA: "this is the wrong answer",
+    choiceB: "this is the right answer",
+    choiceC: "this is the wrong answer",
+    choiceD: "this is the wrong answer",
+    correct: "B",
+  },
+  {
+    question: "This is question 5",
+    choiceA: "this is the wrong answer",
+    choiceB: "this is the wrong answer",
+    choiceC: "this is the wrong answer",
+    choiceD: "this is the right answer",
+    correct: "D",
+  },
 ];
